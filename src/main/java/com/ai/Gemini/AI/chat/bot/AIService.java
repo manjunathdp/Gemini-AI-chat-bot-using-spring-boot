5d18cf196cb05774cc68b7c905b5ccd2ea9de422
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-12-24 18:51:56 +0530

    gemini ai API working back-end part completed

--- src/main/java/com/ai/Gemini/AI/chat/bot/AIService.java
+++ src/main/java/com/ai/Gemini/AI/chat/bot/AIService.java
@@ -2,6 +2,9 @@ package com.ai.Gemini.AI.chat.bot;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
 
 @Service
 public class AIService {
@@ -10,16 +13,37 @@ public class AIService {
     String apiUrl;
     @Value("${api.key}")
     String apiKey;
+
+    private final WebClient webClient;
+
+    public AIService(WebClient.Builder webClient) {
+        this.webClient = webClient.build();
+    }
+
+
     public String getAnswer(String question) {
 //        {
-//            "contents": [{
-//            "parts":[{
-//                "text":"Explain how AI works"
-//            }]
-//        }]
+//            "contents": [{"parts":[{"text":"Explain how AI works"}]}]
 //        }
+        Map<String, Object> responseBody = Map.of(
+                "contents", new Object[]{
+                        Map.of(
+                                "parts", new Object[]{
+                                        Map.of(
+                                                "text", question
+                                        )
+                                }
+                        )
+                }
+        );
 
 
-       return "request reached";
+        return webClient.post()
+                .uri(apiUrl + apiKey)
+                .header("Content-Type", "application/json")
+                .bodyValue(responseBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
     }
 }
